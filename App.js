107a8d562daabe09623622b2533f089cbe212946
Earth_saharat <socool2548@gmail.com>
2022-06-29 11:04:38 +0700

    29-6-22

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import User from './components/User'
+import Logo from './components/Logo'
 
 const App = () => {
   return (
+    // <Logo/>
     <User/>
   )
 }
