d9a41fcff7712af001d2c775a9b37a1e3fb6afac
Earth_saharat <socool2548@gmail.com>
2022-07-20 09:29:25 +0700

    20-7-65

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Count from './components/Count'
-import InputText from './components/InputText'
-import UserNameandPassWord from './components/UserNameandPassWord'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Count from "./components/Count";
+import InputText from "./components/InputText";
+import UserNameandPassWord from "./components/UserNameandPassWord";
+import UserNamePassword from "./components/UserNamePassword";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UserNameandPassWord/>
+      <UserNamePassword/>
+      {/* <UserNameandPassWord /> */}
       {/* <InputText/> */}
       {/* <Count/>
       <LotsOfGreeting/> */}
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  containar :{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-})
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  textInputStyle: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 15,
+  },
+});
